bb73a717b0dffb6633311f7a9a45a5aba65d4625
carimore <110664183+carimore@users.noreply.github.com>
2022-11-17 12:00:59 +0100

    path to books and id working

--- server.js
+++ server.js
@@ -1,30 +1,57 @@
-import express from "express";
+import express, { request } from "express";
 import cors from "cors";
-import technigoMembers from "./data/technigo-members.json"
+import booksData from "./data/books.json"
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/members", (req, res) => {
-  res.status(200).json({technigoMembers: technigoMembers});
-});
+app.get("/", (req, res) => {
+  res.json({responseMessage: "Hello bookworm!"});
+    });
 
-app.get("/members/:id", (req, res) => {
-  const singleMember = technigoMembers.find((member) => {
-    return member.id === Number(req.params.id);
+app.get("/books", (req, res) => {
+  const { tittle, authors } = req.query;
+  let books = booksData
+  if(authors) {
+    books = books.filter(singleBook => singleBook.authors.toLowerCase() === authors.toLowerCase());
+  }
+  if (tittle) {
+    books = books.filter(singleBook => singleBook.tittle.toLowerCase() === tittle.toLowerCase());
+  }
+  
+  res.status(200).json({
+    success: true,
+    message: "OK",
+    body: {booksData: books}
   });
-  res.status(200).json(singleMember);
 });
 
-// Start the server
+app.get('/books/:id', (req, res) => {
+  const singleBook = booksData.find((item) => {
+    return (item.id === +req.params.id)
+  });
+  console.log(singleBook)
+  res.json ({booksData : singleBook})
+  if(bookSpecific) {
+    res.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        booksData: books
+      }
+    });
+  } else {
+    response.status(404).json({
+      success: false,
+      message: "Not Found",
+      body: {}
+    })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
